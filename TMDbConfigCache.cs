6331cd86aa6aeb2495f61bdb75af651ec0b55892
VPKSoft <vpksoft@vpksoft.net>
2018-08-06 19:29:03 +0300

    Initial commit

---
+++ TMDbConfigCache.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using TMdbEasy;
+using TMdbEasy.ApiInterfaces;
+using TMdbEasy.TmdbObjects.Configuration;
+using VPKSoft.Utils;
+
+namespace TMDbFileUtils
+{
+    /// <summary>
+    /// A helper class to store a TMDb configuration to a XML file, so it doesn't need to be requested all the time a software starts executing.
+    /// </summary>
+    public static class TMDbConfigCache
+    {
+        // the TMdbEasy.ApiInterfaces.IConfigApi interface.. 
+        private static IConfigApi configApi;
+
+        /// <summary>
+        /// Gets the configuration path for Windows.Forms application.
+        /// </summary>
+        /// <value>
+        /// The configuration path for Windows.Forms application
+        /// </value>
+        public static string ConfigPathWinforms
+        {
+            get
+            {
+                // first ensure that the path exists..
+                Paths.MakeAppSettingsFolder(Misc.AppType.Winforms);
+
+                // return the path..
+                return Paths.GetAppSettingsFolder(Misc.AppType.Winforms);
+            }
+        }
+
+        /// <summary>
+        /// Gets the configuration path for a WPF application.
+        /// </summary>
+        /// <value>
+        /// The configuration path for a WPF application
+        /// </value>
+        public static string ConfigPathWPF
+        {
+            get
+            {
+                // first ensure that the path exists..
+                Paths.MakeAppSettingsFolder(Misc.AppType.WPF);
+
+                // return the path..
+                return Paths.GetAppSettingsFolder(Misc.AppType.WPF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the TMDb API configuration from the TMdbEasy client.
+        /// </summary>
+        /// <param name="easy">An instance to a TMdbEasy client.</param>
+        /// <param name="configFileName">A name of the configuration file.</param>
+        /// <returns>A Configurations class instance from the TMdbEasy client class instance.</returns>
+        private static Configurations GetApiConfig(EasyClient easy, string configFileName)
+        {
+            configApi = easy.GetApi<IConfigApi>().Value; // get the API..
+
+            // get the Configurations class instance synchronously.. 
+            Configurations configurations = configApi.GetConfigurationAsync().Result;
+
+            // serialize the Configurations class instance to a XML document..
+            XmlDocument xmlDocument = ObjectSerialization.ToXmlDocument(configurations);
+
+            // save the XML document..
+            xmlDocument.Save(configFileName);
+
+            // return the Configurations class instance..
+            return configurations;
+        }
+
+        /// <summary>
+        /// Gets the TMDb API configuration either from the TMdbEasy client or 
+        /// deserialized from a XML document if the document is newer than 7 days old and the XML document exists.
+        /// </summary>
+        /// <param name="easy">An instance to a TMdbEasy client.</param>
+        /// <param name="configPath">The configuration path.</param>
+        /// <returns>A Configurations class instance.</returns>
+        public static Configurations GetConfigurations(EasyClient easy, string configPath)
+        {
+            // construct a XML file name for serialization / deserialization..
+            string configFileName = Path.Combine(configPath, "TMDbConfig.xml");
+            if (File.Exists(configFileName)) // if the XML file exists..
+            {
+                // ..read the XML file's information..
+                FileInfo fileInfo = new FileInfo(configFileName);
+
+                // if the file is newer than 7 days..
+                if ((DateTime.Now - fileInfo.LastWriteTimeUtc).TotalDays < 7)
+                {
+                    // create an instance of a XmlDocument class..
+                    XmlDocument xmlDocument = new XmlDocument();
+
+                    // load the XML file into the XmlDocument class instance..
+                    xmlDocument.Load(configFileName);
+
+                    // deserialize the XML document to a Configurations class instance and return it..
+                    return (Configurations)ObjectSerialization.DeserializeObject(typeof(Configurations), xmlDocument);
+                }
+                else // too old XML file..
+                {
+                    // ..so re-get the configuration and save it to a XML file and return a Configurations class instance..
+                    return GetApiConfig(easy, configFileName);
+                }
+            }
+            else // file doesn't exist..
+            {
+                // ..so get the configuration and save it to a XML file and return a Configurations class instance..
+                return GetApiConfig(easy, configFileName);
+            }
+        }
+    }
+}
---
+++ TMDbFileEnumerator.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TMdbEasy;
+using TMdbEasy.ApiInterfaces;
+using TMdbEasy.TmdbObjects.Movies;
+using TMdbEasy.TmdbObjects.TV;
+using VPKSoft.Utils;
+
+namespace TMDbFileUtils
+{
+    /// <summary>
+    /// A class to contain a single movie or TV show details. This is used by the static TMDbFileEnumerator class.
+    /// </summary>
+    public class TMDbDetail
+    {
+        /// <summary>
+        /// A TMDb movie or TV show ID.
+        /// </summary>
+        public int ID { get; set; } = -1;
+
+        /// <summary>
+        /// A TMDb TV show season ID.
+        /// </summary>
+        public int SeasonID { get; set; } = -1;
+
+        /// <summary>
+        /// A TMDb TV show episode ID.
+        /// </summary>
+        public int EpisodeID { get; set; } = -1;
+
+        /// <summary>
+        /// A TMDb movie or TV show title.
+        /// </summary>
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// A TMDb movie or TV show description.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// A TMDb TV show episode description.
+        /// </summary>
+        public string DetailDescription { get; set; } = string.Empty;
+
+        /// <summary>
+        /// A full path to a local file.
+        /// </summary>
+        public string FileName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// An Uri containing the URL of the TMDb movie or TV show season image.
+        /// </summary>
+        public Uri PosterOrStillURL { get; set; } = null;
+
+        /// <summary>
+        /// A TMBd season number of a TV show.
+        /// </summary>
+        public int Season { get; set; } = 0;
+
+        /// <summary>
+        /// A TMDB episode number of a TV show.
+        /// </summary>
+        public int Episode { get; set; } = 0;
+    }
+
+    /// <summary>
+    /// A class to enumerate files from the file system and then get details for these files from the TMDb database.
+    /// </summary>
+    public static class TMDbFileEnumerator
+    {
+        /// <summary>
+        /// Gets details for movie files from a given path from the TMDb database.
+        /// </summary>
+        /// <param name="easy">An instance to a TMdbEasy.EasyClient class instance.</param>
+        /// <param name="path">A path to enumerate the files from. Do note that these files names should only contain the original title of the movie.</param>
+        /// <param name="posterSize">The size of the poster to get the URL for.</param>
+        /// <returns>A collection of TMDbDetail class instances containing the movie information from the TMDb database.</returns>
+        public static IEnumerable<TMDbDetail> GetMovies(EasyClient easy, string path, string posterSize = "original")
+        {
+            var movieApi = easy.GetApi<IMovieApi>().Value; // create a IMovieApi..
+
+            // List files of known video formats from the given path..
+            IEnumerable<FileEnumeratorFileEntry> fileEntries = FileEnumerator.RecurseFiles(path, FileEnumerator.FiltersVideoVlcNoBinNoIso);
+
+            // loop through the files and try to get a details for them..
+            foreach (FileEnumeratorFileEntry entry in fileEntries)
+            {
+                // query the movie from the TMDb database..
+                MovieList list = movieApi.SearchMoviesAsync(entry.FileNameNoExtension).Result;
+                
+                // if something was found..
+                if (list != null && list.Total_results > 0)
+                {
+                    yield return new TMDbDetail() // return the details of the movie..
+                    {
+                        ID = list.Results[0].Id, // the first result is only taken into account..
+                        Title = list.Results[0].Title, // set the title..
+                        Description = list.Results[0].Overview, // set the overview..
+                        FileName = entry.FileName, // set the file name..
+
+                        // create an Uri for the poster path..
+                        PosterOrStillURL = new Uri("https://image.tmdb.org/t/p/" + posterSize + list.Results[0].Poster_path)
+                    };
+                }
+                else // nothing was found..
+                {
+                    yield return new TMDbDetail()
+                    {
+                        Title = entry.FileNameNoExtension, // the title can be the file name without path or extension..
+                        Description = entry.FileNameNoExtension, // the description can be the file name without path or extension..
+                        FileName = entry.FileName // set the file name..
+                    };
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes the season information from a given path. E.g. SomeShow Season 1.
+        /// </summary>
+        /// <param name="path">A path to make a valid search string from.</param>
+        /// <returns>A string which should be suitable search string for the TMDb for a TV show.</returns>
+        private static string GetTVShowSearchString(string path)
+        {
+            string searchString = new DirectoryInfo(path).Name; // the last path part..
+            searchString = // multiple regular expressions as the programmer is not a guru with them..
+                Regex.Replace(searchString, @"Season [0-9]+", string.Empty, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant).Trim();
+            searchString =
+                Regex.Replace(searchString, @"S [0-9]+", string.Empty, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant).Trim();
+            searchString =
+                Regex.Replace(searchString, @"S[0-9]+", string.Empty, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant).Trim();
+            searchString =
+                Regex.Replace(searchString, @"[0-9]+", string.Empty, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant).Trim();
+            return searchString; // return the string..
+        }
+        
+        /// <summary>
+        /// Gets a TV show episode number of a given path string.
+        /// </summary>
+        /// <param name="path">A path string to get the episode number from.</param>
+        /// <returns>A TV show episode number.</returns>
+        private static int GetTVShowEpisodeNumber(string path)
+        {
+            string searchString = new DirectoryInfo(path).Name; // the last path part..
+
+            string episodeNum = string.Empty; // first assume empty
+
+            // multiple regular expressions as the programmer is not a guru with them..
+            if ((episodeNum = Regex.Match(searchString, @"E\d+", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant).Value).TrimStart('E', 'e') != string.Empty)
+            {
+                if (int.TryParse(episodeNum, out _)) // check for a valid integer..
+                {
+                    return int.Parse(episodeNum); // ..if the integer was valid, return it..
+                }
+            }
+            if ((episodeNum = Regex.Match(searchString, @"X\d+", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant).Value).TrimStart('X', 'x') != string.Empty)
+            {
+                if (int.TryParse(episodeNum, out _)) // check for a valid integer..
+                {
+                    return int.Parse(episodeNum); // ..if the integer was valid, return it..
+                }
+            }
+
+            if ((episodeNum = Regex.Match(searchString, @"\d+", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant).Value) != string.Empty)
+            {
+                if (int.TryParse(episodeNum, out _)) // check for a valid integer..
+                {
+                    return int.Parse(episodeNum); // ..if the integer was valid, return it..
+                }
+            }
+            return -1; // just return -1..
+        }
+        
+        /// <summary>
+        /// Gets the TV show season number from a given path string.
+        /// </summary>
+        /// <param name="path">A path string to extract the season number from</param>
+        /// <returns></returns>
+        private static int GetTVShowSeason(string path)
+        {
+            string searchString = new DirectoryInfo(path).Name; // the last path part..
+
+            searchString = Regex.Match(searchString, @"-?\d+").Value; // find a number from the path part..
+            if (int.TryParse(searchString, out _)) // check for a valid integer..
+            {
+                return int.Parse(searchString); // ..if the integer was valid, return it..
+            }
+            else
+            {
+                return -1; // else just return -1..
+            }
+        }
+
+        /// <summary>
+        /// A list of possible naming conventions for a TV show episode file name.
+        /// </summary>
+        public static List<string> EpisodeNamingStyles { get; set; } = 
+            new List<string>(
+                new string[] 
+                {
+                    "s{0:00}e{1:00}", // SomeShow S01E01.*
+                    "s{0}e{1}",       // SomeShow S1E1.*
+                    "s{0}e{1:00}",    // SomeShow S1E01.*
+                    "s{0:00}e{1}",    // SomeShow S01E1.*
+                    "{0:00}x{1:00}",  // SomeShow 01x01.*
+                    "{0}x{1:00}",     // SomeShow 1x01.*
+                    "{0}x{1}",        // SomeShow 1x1.*
+                    "{0:00}x{1}"      // SomeShow 01x1.*
+                });
+
+        /// <summary>
+        /// Finds a matching file name from a give TV show season and episode number.
+        /// </summary>
+        /// <param name="fileEntries">A collection of FileEnumeratorFileEntry class instances to search for a match.</param>
+        /// <param name="season">A season number to be used in the search.</param>
+        /// <param name="episode">An episode number to be used in the search.</param>
+        /// <returns>A matching file name from a give TV show season and episode number from the given collection.</returns>
+        private static FileEnumeratorFileEntry GetFileNameMatchingTVSeasonEpisode(IEnumerable<FileEnumeratorFileEntry> fileEntries, int season, int episode)
+        {
+            FileEnumeratorFileEntry result = null; // nothing might be found..
+            foreach (string namingStyle in EpisodeNamingStyles) // loop through the given naming styles..
+            {
+                try // as the string format might be invalid, lets not cause the operation to fail because of that..
+                {
+                    result = // try to get a result from the given collection..
+                        fileEntries.FirstOrDefault(f => f.FileNameNoExtension.ToLowerInvariant().Contains(string.Format(namingStyle, season, episode)));
+                    if (result != null) // if found..
+                    {
+                        return result; // ..return it..
+                    }
+                }
+                catch // just an empty catch - there is no hope..
+                {
+
+                }
+            }
+            result = // last try is just the episode number..
+                fileEntries.FirstOrDefault(f => f.FileNameNoExtension.Contains(episode.ToString()));
+
+            return result; // return the result whether it's null or not..
+        }
+
+        /// <summary>
+        /// A format of how to give a title to a TV show episode. It must have four parameters.
+        /// The format is as {0} is the name of the series, {1} is the name of the season, {2} is the number of the episode
+        /// and {3} is the name of the episode.
+        /// <note type="note">Exceptions will occur if the format is incorrect.</note>
+        /// </summary>
+        public static string TVEpisodeFormat { get; set; } = "{0} {1}, Episode {2} - {3}";
+
+        /// <summary>
+        /// Searches the TMDb database for a TV season based on a given path using a TMdbEasy.EasyClient class instance.
+        /// </summary>
+        /// <param name="easy">A TMdbEasy.EasyClient class instance to use for the search.</param>
+        /// <param name="path">A path to enumerate files from.</param>
+        /// <param name="stillSize">The size of the still image to get the URL for.</param>
+        /// <returns>A collection of TMDbDetail class instances containing the TV show season information from the TMDb database.</returns>
+        public static IEnumerable<TMDbDetail> GetSeason(EasyClient easy, string path, string stillSize = "original")
+        {
+            var televisionApi = easy.GetApi<ITelevisionApi>().Value; // create a ITelevisionApi..
+
+            // List files of known video formats from the given path..
+            IEnumerable<FileEnumeratorFileEntry> fileEntries = FileEnumerator.RecurseFiles(path, FileEnumerator.FiltersVideoVlcNoBinNoIso);
+
+            // don't start searching if the directory is empty - we don't want to cause excess stress for the TMDb database..
+            if (fileEntries.ToList().Count == 0)
+            {
+                // ..so just throw an exception..
+                throw new Exception("No files were found from the given path string.");
+            }
+
+            // construct a search string of the given path..
+            string searchString = GetTVShowSearchString(path);
+            int season = GetTVShowSeason(path); // extract a season number from the given path..
+
+            if (season == -1) // if no season number was in the given path..
+            {
+                // ..just throw an exception..
+                throw new Exception("The TV season number wasn't found of the given path string.");
+            }
+
+            // search for TV shows base on the search string build from the given directory name..
+            TVShowList list = televisionApi.SearchTVShowsAsync(searchString).Result;
+
+            // if something was found..
+            if (list != null && list.Total_results > 0) // ..deepen the search..
+            {
+                string seriesName = list.Results[0].Name; // save the name of the TV show..
+                int seriesID = list.Results[0].Id; // save the ID of the TV show..
+
+                // find the TV show season from the TMDb database with an ID and a season number..
+                TvSeason tvSeason = televisionApi.GetSeasonDetailsAsync(seriesID, season).Result;
+
+                // if something was found..
+                if (tvSeason != null && tvSeason.Episodes != null)
+                {
+                    foreach (Episode episode in tvSeason.Episodes) // return the details of the TV show season..
+                    {
+                        // don't return a file-less TMDbDetail class instance..
+                        if (GetFileNameMatchingTVSeasonEpisode(fileEntries, season, episode.Episode_number) == null)
+                        {
+                            continue; // ..so just continue the loop..
+                        }
+
+                        yield return new TMDbDetail
+                        {
+                            ID = seriesID, // the TMDb id for the TV show..
+                            SeasonID = tvSeason.Id, // the TMDb id for the TV show season..
+                            EpisodeID = episode.Id, // the TMDb id for the TV show season episode..
+
+                            // formulate the title base on the TVEpisodeFormat property value..
+                            Title = string.Format(TVEpisodeFormat, seriesName, tvSeason.Name, episode.Episode_number, episode.Name),
+
+                            // set the description..
+                            Description = string.IsNullOrEmpty(tvSeason.Overview) ? episode.Overview : tvSeason.Overview,
+                            DetailDescription = episode.Overview, // set the detailed description if any..
+
+                            // find the file name for the TV show episode..
+                            FileName = GetFileNameMatchingTVSeasonEpisode(fileEntries, season, episode.Episode_number).FileName,
+
+                            // create an URL for the still using the TV season's poster path as a "fail safe"..
+                            PosterOrStillURL = new Uri("https://image.tmdb.org/t/p/" + stillSize + 
+                                (string.IsNullOrEmpty(episode.Still_path) ? tvSeason.Poster_path : episode.Still_path)),
+                            Season = season, // set the season number..
+                            Episode = episode.Episode_number // set the episode number..
+                        };
+                    }
+                }
+                else // nothing was found..
+                {
+                    // loop through the found files..
+                    foreach (FileEnumeratorFileEntry entry in fileEntries)
+                    {
+                        yield return new TMDbDetail
+                        {
+                            Title = entry.FileNameNoExtension, // the title can be the file name without path or extension..
+                            FileName = entry.FileName, // set the file name..
+                            Season = season, // set the season number.. 
+                            Episode = GetTVShowEpisodeNumber(entry.FileName) // set the episode number..                
+                        };
+                    }
+                }
+            }
+        }
+    }
+    
+}
