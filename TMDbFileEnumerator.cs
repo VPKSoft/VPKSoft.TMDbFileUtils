a1af9b2d11a1edfb772cb8bfa8aa08df349395b2
VPKSoft <vpksoft@vpksoft.net>
2018-08-09 16:38:17 +0300

    Added license region.

--- TMDbFileEnumerator.cs
+++ TMDbFileEnumerator.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿#region License
+/*
+VPKSoft.TMDbFileUtils
+
+A library to help enumerate both movie files and directories containing TV show seasons and the run them through the TMBb API.
+Copyright © 2018 VPKSoft, Petteri Kautonen
+
+Contact: vpksoft@vpksoft.net
+
+This file is part of VPKSoft.TMDbFileUtils.
+
+VPKSoft.TMDbFileUtils is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+VPKSoft.TMDbFileUtils is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with VPKSoft.TMDbFileUtils.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
